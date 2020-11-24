1f2eab844a83b5853a993df4c4c7fb50dddad601
esdeath11 <esdeath11@users.noreply.github.com>
2020-11-24 13:13:23 +0700

    sprint 2 new update

---
+++ Script/Controller/Navigation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour
+{
+    [SerializeField] GameObject bag;
+
+    public void inventory()
+    {
+        bag.SetActive(true);
+    }
+
+    public void closeInventory()
+    {
+        bag.SetActive(false);
+    }
+
+
+}
