0ba94d88fdc7db7b596f6f820ad536c7f2fa7db6
esdeath11 <esdeath11@users.noreply.github.com>
2020-11-24 13:21:08 +0700

    sprint 2 fix shop(progress)

--- Script/Controller/Inventory/Shop.cs
+++ Script/Controller/Inventory/Shop.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine.UI;
+using System;
 
 public class Shop : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Text textMoney;
+    int money;
+    DatabaseReference reference;
     void Start()
     {
-        
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        loadData();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void loadData()
     {
-        
+        FirebaseDatabase.DefaultInstance
+        .GetReference("Shop")
+        .ValueChanged += Database_ValueChanged;
+    }
+
+    private void Database_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        textMoney.text ="Rp. " + e.Snapshot.Child("Money").GetValue(true).ToString();
     }
 }
